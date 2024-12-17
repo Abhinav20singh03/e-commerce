89f18c7091322a7c231ecf3a661f31d33a121bc3
Abhinav20singh03 <abhinav20singh03@gmail.com>
2024-12-18 05:00:10 +0530

    routing done

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
-import Navbar from './components/navbar'
 import './App.css'
-
+import {Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
 function App() {
 
   return (
     <div className='home-page'>
       <Navbar/>
-      
+      <Routes>
+        <Route path="/" element={<Home/>} /> 
+        <Route path="/about" element={<About/>} /> 
+        <Route path="/cart" element={<Cart/>} /> 
+        <Route path="/collection" element={<Collection/>} /> 
+        <Route path="/contact" element={<Contact/>} />  
+        <Route path="/orders" element={<Orders/>} />  
+        <Route path="/placeorder" element={<PlaceOrder/>} />  
+        <Route path="/product/:productId" element={<Product/>} /> 
+        <Route path="/login" element={<Login/>} />  
+      </Routes>
     </div>
   )
 }
--- Project/src/components/Navbar.jsx
+++ Project/src/components/Navbar.jsx
@@ -1,96 +1,45 @@
-import React, {useEffect,useState,Component } from 'react'
-import { MdOutlineSegment } from "react-icons/md";
-import { FaCartShopping } from "react-icons/fa6";
-import { FaUserCircle } from "react-icons/fa";
+import React, { useEffect, useState } from 'react'
 import "./Navbar.css"
-import SideNav from './Sidenav';
-function Navbar(){
+import { NavLink } from 'react-router-dom'
+import { assets  } from '../assets/assets'
+import { useNavigate } from "react-router-dom";
+const Navbar = () => {
 
-    const[sideToggle,setSideToggle] = useState(false);
-
-    const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
-
-    // Update viewport width when the window is resized
-    useEffect(() => {
-      const handleResize = () => {
-        setViewportWidth(window.innerWidth);
-      };
-  
-      // Add event listener to handle window resize
-      window.addEventListener('resize', handleResize);
-  
-      // Cleanup the event listener on component unmount
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }, []);
-
-
-    const [isToggled, setIsToggled] = useState(false);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-          setIsToggled(prevState => !prevState); // Toggle state
-        }, 5000); // 3000ms = 3 seconds
+    const [searchBar,setSearchBar] = useState(false);
+    const [SideNavbar,setSideNavbar] = useState(false);
     
-        return () => clearInterval(intervalId);
-      }, []);
-    
+    const navigate = useNavigate();
+  return (
+    <>
+        <div className='container'>
+        <img src={assets.logo} className='logo'/> 
+        <div className='pages'>
+            <NavLink to="/" className='page'>Home</NavLink>
+            <NavLink to="/collection" className='page'>Collection</NavLink>
+            <NavLink to="/about" className='page'>About</NavLink>
+            <NavLink to="/contact" className='page'>Contact</NavLink>
+        </div>  
+        <div className='navigation-features'>
+            <img className="features" onClick={()=>{navigate("/login")}} src={assets.profile_icon}/>
+            <img className="features" onClick={()=>{navigate("/cart")}} src={assets.cart_icon}/>
+            <img className="features" onClick={()=>{setSearchBar(prev=>!prev)}} src={assets.search_icon}/>
 
-   
-
-    return (
-
-        <div className='nav-container'>
-
-            <SideNav/>
-            <div className="navbar-1">
-                <div className="search-bar">
-                    <input type="text" placeholder="Search..." />
-                </div>
-                <div className="icons">
-                    <a href="#" className="icon cart-icon"><FaCartShopping /></a>
-
-                    {
-                        viewportWidth < 768 ? 
-                        <>
-                        
-                        </>
-                        :
-                        <>
-                        <a href="#" className="icon login-icon"><FaUserCircle /></a>
-                        </>
-                    }
-                </div>
-            </div>
-            <div className='navbar-2'>
-                <div className='left-part'>
-                    <div className='left-part-1'>THRIFT TREASURES</div>
-                    <div className='left-part-2'>
-                    {
-                        viewportWidth < 768 ? 
-                        <>
-                        <MdOutlineSegment onClick={()=>{
-                            setSideToggle(true);
-                        }} />
-                        </>
-                        :
-                        <>
-                        <div>Shop</div>
-                        <div>About</div>
-                        <div>FAQ</div>
-                        <div>Contact</div>
-                        </>
-                    }    
-                    </div>
-                </div>
-                <div className='right-part'>
-                {isToggled ? "contact at 101" : "hello@gamil"}
-                </div>
-            </div>
+            <img className='features' onClick={()=>{setSideNavbar(true)}}/>
+        </div>  
+    </div>
+    {
+        searchBar ? 
+        <>
+        <div className='search-bar-container'>
+        <input type='text' placeholder='Search' className="search-bar"/>
         </div>
-            
+        </> : 
+        <></>
+    }
 
-    )
+    </>
+    
+  )
 }
-export default Navbar;
+
+export default Navbar
